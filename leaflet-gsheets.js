61d3a5f5e11008c4188fa7b563ae9086069a5624
John Zdragas <std091649@ac.eap.gr>
2020-06-19 13:29:30 +0300

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -5,7 +5,7 @@
  * The Sheets are then imported using Tabletop.js and overwrite the initially laded layers
  */
 
-var mycenterPosition = new L.LatLng(41, 21);
+var mycenterPosition = new L.LatLng(42, 21);
 
 // init() is called as soon as the page loads
 function init() {
