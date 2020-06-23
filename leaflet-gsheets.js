23c0fd659928f4e2da3bedcf7169ba72b3c40163
John Zdragas <std091649@ac.eap.gr>
2020-06-23 13:02:08 +0300

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -4,7 +4,8 @@
  * Script to display two tables from Google Sheets as point and polygon layers using Leaflet
  * The Sheets are then imported using Tabletop.js and overwrite the initially laded layers
  */
-
+var center_Point = new L.LatLng(40, 20);
+alert (center_Point.toString);
 // init() is called as soon as the page loads
 function init() {
   // PASTE YOUR URLs HERE
