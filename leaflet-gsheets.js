58593d380d93d64125bde6304c42b4568a689a9e
John Zdragas <std091649@ac.eap.gr>
2020-06-23 09:25:20 +0300

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -203,7 +203,7 @@ function addPoints(data) {
       prefix: "glyphicon",
       extraClasses: "fa-rotate-0"
     });*/
-    var icon = L.AwesomeMarkers.icon({icon: 'fa-warehouse', prefix: 'fa', markerColor: 'red', iconColor: '#f28f82'});
+    var icon = L.AwesomeMarkers.icon({icon: 'bookmark', prefix: 'fa', markerColor: 'red', iconColor: 'darkpurple'});
     if (!markerType.includes("circle")) {
       marker.setIcon(icon);
     }
