d98973fef24ea412605b59eef90fc363b9ae33f1
John Zdragas <std091649@ac.eap.gr>
2020-06-23 08:32:14 +0300

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -21,7 +21,7 @@ function init() {
 window.addEventListener("DOMContentLoaded", init);
 
 // Create a new Leaflet map centered on the continental US
-var map = L.map("map").setView([40, 20], 1);
+var map = L.map("map").setView([40, 20], 3);
 map.locate({setView: true, maxZoom: 6});
 // Για να προσθέσουμε την δυνατότητα εντοπισμού της θέσης μας και να τοποθετήσουμε και έναν marker στη θέση μας:
 
