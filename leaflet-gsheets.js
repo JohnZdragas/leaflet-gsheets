99925b4e3abaaccafae9a01a207ef6702e58d50f
John Zdragas <std091649@ac.eap.gr>
2020-06-23 13:42:58 +0300

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -199,7 +199,7 @@ function addPoints(data) {
   for (var row = 0; row < data.length; row++) {
     var pointToConsider = new L.LatLng(data[row].lat, data[row].lon);
     var poso_Distance = center_Point.distanceTo(pointToConsider);
-    if (poso_Distance < 20000) {
+    if (poso_Distance < 50000) {
     alert (poso_Distance);
     }
     var marker;
